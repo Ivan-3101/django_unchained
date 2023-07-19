aa31a0165be78ca7054d377507e37bfa869f5128
Ivan Dsouza <113847518+Ivan-3101@users.noreply.github.com>
2023-07-20 04:35:49 +0530

    used djangos form instead of html

--- django_unchained/tasks/views.py
+++ django_unchained/tasks/views.py
@@ -3,7 +3,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+from django import forms
+
 tasks_list = ["check email","backup files","pray"]
+
+class NewTaskForm_classname(forms.Form):
+    # ALL THE FIELDS ID LIKE THE USER TO HAVE
+    task = forms.CharField(label="New Task :")
+
 
 def index(request):
     return render(request,"tasks/index.html",{
@@ -11,7 +18,15 @@ def index(request):
     })
 
 def add(request):
-    return render(request,"tasks/add.html")
+    return render(request,"tasks/add.html",{
+        "var_form": NewTaskForm_classname()
+        # give this template access
+        # to a variable called form which will be a new task form
+    
+        # now instead of using the html form syntax
+        # we use this {{ var_form }}
+    
+    })
 
 def ivan(request,name):
     return render(request,"tasks/ivan.html",{
