e798e85f43e4d334583a2bf955021484931676d5
Ivan Dsouza <113847518+Ivan-3101@users.noreply.github.com>
2023-07-20 03:52:37 +0530

    added the list of tasks and inherited the template for hello

--- django_unchained/tasks/views.py
+++ django_unchained/tasks/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-# def index(request):
-#     return HttpResponse("Hello Ivan")
+tasks_list = ["check email","backup files","pray"]
+
+def index(request):
+    return render(request,"tasks/index.html",{
+        "tasks":tasks_list,
+    })
 
 def ivan(request,name):
     return render(request,"tasks/ivan.html",{
